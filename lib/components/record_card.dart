5da6b7bb9b5095fc55dfac8eb8746dcddfa5940e
Palladiusz <palladiusz.ra@gmail.com>
2020-08-07 10:14:28 +0200

    stylization record card

--- lib/components/record_card.dart
+++ lib/components/record_card.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:intl/intl.dart';
 
 class RecordCard extends StatelessWidget {
+  final DateTime date = DateTime.now();
+  var day = DateFormat.yMd().format(DateTime.now());
+  var hour = DateFormat.Hm().format(DateTime.now());
   @override
   Widget build(BuildContext context) {
     return Neumorphic(
@@ -12,15 +16,36 @@ class RecordCard extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              '${DateTime.now()}',
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '$day',
+                ),
+                Text(
+                  '$hour',
+                )
+              ],
             ),
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 Text('Bid: 20'),
                 Row(
-                  children: <Widget>[Text('100'), Text('--->'), Text('150')],
+                  children: <Widget>[
+                    Text('100'),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      Icons.arrow_forward,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('150')
+                  ],
                 ),
                 Text('+50')
               ],
